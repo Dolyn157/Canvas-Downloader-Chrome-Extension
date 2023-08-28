40f75f446421acdbddfa2f8e728ba3b395d62815
Dolyn157 <83268912+Dolyn157@users.noreply.github.com>
2023-08-28 10:23:52 +0800

    getCanvasByClass() added

--- src/content.js
+++ src/content.js
@@ -3,6 +3,10 @@ chrome.runtime.onMessage.addListener((request , sender , sendResponse) => {
     if(request.action == "DOWNLOADALL") {
         let canvases = getAllCanvas()
         downLoadCanvas(canvases);
+    }else if (request.action == "DOWNLOADCANVASBYCLASS"){
+        console.log(request.payload + ' from popup')
+        let canvases = getCanvasByClass(request.payload)
+        downLoadCanvas(canvases);
     }else{
         console.log("这不是点击！");
     }
@@ -12,7 +16,7 @@ chrome.runtime.onMessage.addListener((request , sender , sendResponse) => {
    
 function getAllCanvas(){
     var canvases = document.getElementsByTagName("canvas")
-    if (typeof(canvases) == 'undefined'){
+    if (canvases == null){
         alert("在该页面找不到Canvas元素");
         return null;
     }
@@ -24,16 +28,32 @@ function getAllCanvas(){
     return canvases;
 }
 
+function getCanvasByClass(payload){
+    var canvases = document.querySelectorAll(`canvas[class~="${payload}" ]`)
+    if (canvases == null){
+        alert("在该页面找不到Canvas元素");
+        return null;
+    }
+    if (canvases.length == 0){
+        alert("在该页面找不到Canvas元素");
+        return null;
+    }
+    console.log(canvases)
+    return canvases
+}
+
 function downLoadCanvas(canvases){
 
     for (i = 0; i <= canvases.length; i++) {
         const pageCanvas = canvases[i];
         if (pageCanvas === null) { break; }
+        
         const pageNo = parseInt(String(i));
         setTimeout(() => {
             console.log("==pageNo==>>", pageNo);
             ((num) => {
                 console.log("开始打印第" + num + "页");
+                pageCanvas.scrollIntoView()
                 pageCanvas.toBlob(
                     blob => {
                         const anchor = document.createElement('a');
--- src/popup.js
+++ src/popup.js
@@ -8,4 +8,20 @@ btn1.addEventListener("click", async (event) => {
       payload:'Download all canvases in the page.'
     })
   })
+
+
+  btn2.addEventListener("click", async (event) => {
+    let [tab] = await chrome.tabs.query({ 
+      active: true, 
+      currentWindow: true 
+    });
+    let canvasText = document.getElementById('canvasText')
+    let canvasCLASS = canvasText.value
+    
+
+    chrome.tabs.sendMessage(tab.id, {
+      action:'DOWNLOADCANVASBYCLASS' , 
+      payload: canvasCLASS
+    })
+  })
   
